bceb609150514e98b262cdbc25f2f53651db2b3b
mapo-pl <mapoplawski@gmail.com>
2024-12-16 08:47:56 +0100

    Add files via upload

---
+++ lata_do_1500.js
@@ -0,0 +1,25 @@
+var lata_do_1500=
+{
+"type": "FeatureCollection",
+"name": "do_1500",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny par. rzymskokat. pw. św. Andrzeja Apostoła (Spławie)", "ID": "RK45", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.025428, 52.3508645, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny rzymskokat. par. pw. św. Jakuba Większego Apostoła w Głuszynie", "ID": "RK42", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.952063657264599, 52.319105404980014, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przyszpitalny św. Krzyża", "ID": "RK30", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9312896, 52.4048073, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przyklasztorny karmelitów (Piaski)", "ID": "RK27", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9333164, 52.4009958, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny św. Jana Jerozolimskiego za Murami", "ID": "RK16", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9585815, 52.4096298, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny św. Małgorzaty", "ID": "RK14", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.954081, 52.4112943, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz katedralny ", "ID": "RK13", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9478886, 52.4114213, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny NMP", "ID": "RK12", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9471278, 52.4109956, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny św. Wojciecha", "ID": "RK8", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9326341, 52.4134865, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Nowy Cmentarz (parafii farnej) z kolegiatą Wszystkich Świętych", "ID": "RK5", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9363638, 52.4060146, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz kolegiacki (farny)", "ID": "RK4", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9356061, 52.406773499999986, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przykościelny św. Marcina", "ID": "RK1", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9279262, 52.4058093, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przyszpitalny Świętego Ducha", "ID": "RK58", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.933603, 52.4113207, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przyklasztorny katarzynek", "ID": "RK55", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9335408, 52.4104068, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz przyklasztorny dominikanów", "ID": "RK54", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9378399, 52.4101948, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cmentarz par. rzymskokat. pw. Michała Archanioła i Wniebowzięcia Najświętszej Maryi Panny (Kiekrz)", "ID": "RK51", "typ": "rzymskokatolicki", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.7848143, 52.4764981, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Stary cmentarz żydowski", "ID": "J4", "typ": "żydowski", "do1500": 1 }, "geometry": { "type": "Point", "coordinates": [ 16.9262529, 52.4093513, 0.0 ] } }
+]
+}
